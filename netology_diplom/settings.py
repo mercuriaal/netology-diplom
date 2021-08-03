a0c6c3f3b9e2384419ba1b0a8c2e5153b8695d69
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-03 15:49:48 +0300

    secret key moved to .env

--- netology_diplom/settings.py
+++ netology_diplom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w11zmwjt@gm1qov@vwy^l1a)y9u6!@s4hvf6$)rgamv=h%v^13'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
