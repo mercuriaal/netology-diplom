2d9a475ec138b0ea3d91d22ccaf5e028bd33031d
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-04 14:15:57 +0300

    user routes added

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import registration_view, logout_view
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('register/', registration_view, name='register'),
+    path('login/', obtain_auth_token, name='login'),
+    path('logout/', logout_view, name='logout')
+]
