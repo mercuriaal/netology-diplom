4b68e4e198135253cf8739fb78f5b4d51750d324
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-04 12:02:57 +0300

    registration and logout views added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+@api_view(['POST'])
+def registration_view(request):
+
+    serializer = RegisterSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        user = serializer.save()
+        data['response'] = 'Пользователь успешно создан'
+        data['username'] = user.username
+        data['email'] = user.email
+        token = Token.objects.get(user=user).key
+        data['token'] = token
+    else:
+        data = serializer.errors
+    return Response(data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+
+    request.user.auth_token.delete()
+    return Response('Пользователь успешно вышел из системы')
