922d4226ae0804ab14e1f253fa3b456b0df7df00
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-06 19:39:52 +0300

    added validation to shop serializer added category, product and productinfo serializer

--- partners/serializers.py
+++ partners/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Shop
+from .models import Shop, Category, Product, ProductInfo
 from users.serializers import AccountSerializer
 
 
@@ -15,3 +15,38 @@ class ShopSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["user"] = self.context["request"].user
         return super().create(validated_data)
+
+    def validate(self, data):
+        user = self.context["request"].user
+        shop_name = data.get('name')
+        check_user_duplicate = Shop.objects.filter(user=user)
+        check_name_duplicate = Shop.objecta.filter(name=shop_name)
+        if any([check_user_duplicate.exists(), check_name_duplicate.exists()]):
+            raise serializers.ValidationError('Магазин с данным пользователем или именем уже существует.')
+        return data
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['name', 'special_id']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['name', 'category', 'special_id']
+
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+
+    shop = ShopSerializer(read_only=True)
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = ['model', 'quantity', 'price', 'price_rrc', 'shop', 'product']
