c3603821c725eb1f32fdc5b9c88861eda68e4244
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-06 19:39:13 +0300

    product and category views added

--- partners/views.py
+++ partners/views.py
@@ -1,30 +1,57 @@
 import yaml
 from pprint import pprint
 
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from partners.models import Shop, Category, Product, ProductInfo, Parameter, ProductParameter
-from partners.serializers import ShopSerializer
-from users.permissions import Partner
+from partners.serializers import ShopSerializer, CategorySerializer, ProductSerializer
+from users.permissions import Partner, Owner
 
 
 class ShopViewSet(ModelViewSet):
 
-    queryset = Shop.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [Owner, Partner]
     serializer_class = ShopSerializer
+
+    def get_queryset(self):
+        return self.request.user.shop
+
+    def get_permissions(self):
+        if self.action in ['destroy']:
+            return [IsAdminUser()]
+        return [permission() for permission in self.permission_classes]
+
+
+class CategoryView(ListAPIView):
+
+    queryset = Category.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [Owner, Partner]
+    serializer_class = CategorySerializer
+
+
+class ProductView(ListAPIView):
+
+    queryset = Product.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [Owner, Partner]
+    serializer_class = ProductSerializer
 
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated, Partner])
 def upload_products(request):
 
-    user_shop = Shop.objects.filter(user=request.user)
+    user_shop = ShopViewSet.objects.filter(user=request.user)
     if user_shop.exists():
-        path = Shop.objects.get(user=request.user).file
+        path = ShopViewSet.objects.get(user=request.user).file
     else:
         raise ValidationError('Пользователь не привязан к магазину')
 
@@ -42,7 +69,7 @@ def upload_products(request):
                 upload.get('goods')[0].get('price_rrc'), upload.get('goods')[0].get('quantity')]):
         raise ValidationError('В файле отсутствуют необходимые поля')
 
-    shop = Shop.objects.get(user=request.user)
+    shop = ShopViewSet.objects.get(user=request.user)
     for item in upload['categories']:
         category, _ = Category.objects.get_or_create(special_id=item['id'], name=item['name'])
         category.shops.add(shop.id)
