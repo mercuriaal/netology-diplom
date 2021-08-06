f487df43c7ed9d3ff0cfa06d3633a16afe71352c
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-06 19:38:48 +0300

    added category and product urls

--- partners/urls.py
+++ partners/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ShopViewSet, upload_products
+from .views import ShopViewSet, CategoryView, upload_products, ProductView
 
 partners_router = DefaultRouter()
 
 partners_router.register('shop/', ShopViewSet, basename='shops')
 
 urlpatterns = [
-    path('upload/', upload_products, name='upload')
+    path('upload/', upload_products, name='upload'),
+    path('categories/', CategoryView, name='categories'),
+    path('products/', ProductView, name='products')
 ]
