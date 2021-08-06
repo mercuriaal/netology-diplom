c27b2cd2eef9bbb45d180aba1ed76e27593bb406
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-06 16:59:14 +0300

    added validation and restricted permission to upload view

--- partners/views.py
+++ partners/views.py
@@ -9,6 +9,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from partners.models import Shop, Category, Product, ProductInfo, Parameter, ProductParameter
 from partners.serializers import ShopSerializer
+from users.permissions import Partner
 
 
 class ShopViewSet(ModelViewSet):
@@ -18,7 +19,7 @@ class ShopViewSet(ModelViewSet):
 
 
 @api_view(["POST"])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated, Partner])
 def upload_products(request):
 
     user_shop = Shop.objects.filter(user=request.user)
@@ -31,6 +32,16 @@ def upload_products(request):
         upload = yaml.load(file, Loader=yaml.FullLoader)
         pprint(upload)
 
+    if not all([upload.get('categories'), upload.get('goods'), upload.get('shop')]):
+        raise ValidationError('В файле отсутствуют необходимые поля')
+
+    if not all([upload.get('categories')[0].get('id'), upload.get('categories')[0].get('name'),
+                upload.get('goods')[0].get('id'), upload.get('goods')[0].get('category'),
+                upload.get('goods')[0].get('model'), upload.get('goods')[0].get('name'),
+                upload.get('goods')[0].get('parameters'), upload.get('goods')[0].get('price'),
+                upload.get('goods')[0].get('price_rrc'), upload.get('goods')[0].get('quantity')]):
+        raise ValidationError('В файле отсутствуют необходимые поля')
+
     shop = Shop.objects.get(user=request.user)
     for item in upload['categories']:
         category, _ = Category.objects.get_or_create(special_id=item['id'], name=item['name'])
@@ -40,11 +51,13 @@ def upload_products(request):
     for item in upload['goods']:
         category = Category.objects.get(special_id=item['category'])
         product, _ = Product.objects.get_or_create(name=item['name'], category=category, special_id=item['id'])
-        product_info, _ = ProductInfo.objects.get_or_create(model=item['model'], quantity=item['quantity'], price=item['price'],
-                                                  price_rrc=item['price_rrc'], product=product, shop=shop)
+        product_info, _ = ProductInfo.objects.get_or_create(model=item['model'], quantity=item['quantity'],
+                                                            price=item['price'], price_rrc=item['price_rrc'],
+                                                            product=product, shop=shop)
 
         for name, value in item['parameters'].items():
             parameter, _ = Parameter.objects.get_or_create(name=name)
-            ProductParameter.objects.create(product_info=product_info, parameter=parameter)
+            prod_param, _ = ProductParameter.objects.get_or_create(product_info=product_info, parameter=parameter,
+                                                                   value=value)
 
     return Response('Товары загружены в базу данных')
