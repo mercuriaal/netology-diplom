5f5e3ae2e523529b3d00747153affb1b32f0c65e
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-11 01:13:48 +0300

    contact url added

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from .views import registration_view, logout_view
+from .views import registration_view, logout_view, ContactViewSet
 from rest_framework.authtoken.views import obtain_auth_token
+
+contact_router = DefaultRouter()
+
+contact_router.register('contact', ContactViewSet, basename='contact')
 
 urlpatterns = [
     path('register/', registration_view, name='register'),
