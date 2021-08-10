6a9ee1582c0149f88680fb55ea8821d93cfff1b9
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-11 01:15:32 +0300

    contact viewset added

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.viewsets import ModelViewSet
 
-from .serializers import RegisterSerializer
+from .models import Contact
+from .serializers import RegisterSerializer, ContactSerializer
 
 
 @api_view(['POST'])
@@ -31,3 +34,14 @@ def logout_view(request):
 
     request.user.auth_token.delete()
     return Response('Пользователь успешно вышел из системы')
+
+
+class ContactViewSet(ModelViewSet):
+
+    serializer_class = ContactSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get_queryset(self):
+        queryset = Contact.objects.filter(user=self.request.user.id)
+        return queryset
