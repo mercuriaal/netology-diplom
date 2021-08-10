7654ffde015e704f373f686d124ca66c5f9bdcc3
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-11 01:10:21 +0300

    order path added

--- partners/urls.py
+++ partners/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 
-from .views import PartnerStateView, upload_products
-
+from .views import PartnerStateView, upload_products, PartnerOrders
 
 urlpatterns = [
     path('upload/', upload_products, name='upload'),
     path('shop/', PartnerStateView.as_view(), name='shop'),
     path('shop/<int:pk>', PartnerStateView.as_view(), name='shop'),
+    path('order', PartnerOrders.as_view(), name='order')
 ]
