bae02b6e74069ce0d1ce3f1fd59ec91e078eb5d2
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-10 12:54:31 +0300

    permission messages added

--- users/permissions.py
+++ users/permissions.py
@@ -3,17 +3,23 @@ from rest_framework.permissions import BasePermission
 
 class Partner(BasePermission):
 
+    message = 'Только для поставщиков'
+
     def has_permission(self, request, view):
         return request.user.type == 'partner'
 
 
 class Client(BasePermission):
 
+    message = 'Только для клиентов'
+
     def has_permission(self, request, view):
         return request.user.type == 'client'
 
 
 class Owner(BasePermission):
+
+    message = 'Только для владельцев'
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
