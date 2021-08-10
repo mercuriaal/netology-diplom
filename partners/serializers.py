bb61847ba8fef9032e94a9c2edfd38bc17e518b3
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-10 12:52:53 +0300

    set IDs to read only

--- partners/serializers.py
+++ partners/serializers.py
@@ -1,36 +1,22 @@
 from rest_framework import serializers
 
-from .models import Shop, Category, Product, ProductInfo
-from users.serializers import AccountSerializer
+from .models import Shop, Category, Product, ProductInfo, ProductParameter, Parameter
 
 
 class ShopSerializer(serializers.ModelSerializer):
 
-    user = AccountSerializer(read_only=True)
-
     class Meta:
         model = Shop
-        fields = ['user', 'name', 'file']
-
-    def create(self, validated_data):
-        validated_data["user"] = self.context["request"].user
-        return super().create(validated_data)
-
-    def validate(self, data):
-        user = self.context["request"].user
-        shop_name = data.get('name')
-        check_user_duplicate = Shop.objects.filter(user=user)
-        check_name_duplicate = Shop.objecta.filter(name=shop_name)
-        if any([check_user_duplicate.exists(), check_name_duplicate.exists()]):
-            raise serializers.ValidationError('Магазин с данным пользователем или именем уже существует.')
-        return data
+        fields = ('id', 'name', 'state')
+        read_only_fields = ('id', )
 
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['name', 'special_id']
+        fields = ['id', 'name', 'special_id']
+        read_only_fields = ('id',)
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -39,7 +25,8 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['name', 'category', 'special_id']
+        fields = ['id', 'name', 'category', 'special_id']
+        read_only_fields = ('id',)
 
 
 class ProductInfoSerializer(serializers.ModelSerializer):
@@ -49,4 +36,23 @@ class ProductInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductInfo
-        fields = ['model', 'quantity', 'price', 'price_rrc', 'shop', 'product']
+        fields = ['id', 'model', 'quantity', 'price', 'price_rrc', 'shop', 'product']
+        read_only_fields = ('id',)
+
+
+class ProductParameterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductParameter
+        fields = ['id', 'value']
+        read_only_fields = ('id',)
+
+
+class ParameterSerializer(serializers.ModelSerializer):
+
+    product_params = ProductParameterSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Parameter
+        fields = ['id', 'name', 'product_params']
+        read_only_fields = ('id',)
