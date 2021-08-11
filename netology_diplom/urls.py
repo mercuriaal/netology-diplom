e17cfa95e4399ae96656bd7d462ff9e92dcdfd22
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-11 17:11:17 +0300

    added missing slash

--- netology_diplom/urls.py
+++ netology_diplom/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api/v1/partner/', include('partners.urls'), name='partner_api'),
     path('api/v1/client/', include('clients.urls'), name='client_api'),
     path('api/v1/client/', include(basket_router.urls)),
-    path('api/v1/account', include(contact_router.urls))
+    path('api/v1/account/', include(contact_router.urls))
 ]
