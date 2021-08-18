d0af654fdc68d67eda55e5059fb32934536a9649
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-18 23:34:33 +0300

    updated uploadproduct url according to new name

--- partners/urls.py
+++ partners/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 
-from .views import PartnerStateView, upload_products, PartnerOrders
+from .views import PartnerStateView, UploadProduct, PartnerOrders
 
 urlpatterns = [
-    path('upload/', upload_products, name='upload'),
+    path('upload/', UploadProduct.as_view(), name='upload'),
     path('shop/', PartnerStateView.as_view(), name='shop'),
     path('shop/<int:pk>', PartnerStateView.as_view(), name='shop'),
     path('order/', PartnerOrders.as_view(), name='order')
