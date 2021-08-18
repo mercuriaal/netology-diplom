4192a6e9897879f1d8b2d6e7b9a97444f429d3bf
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-18 13:40:59 +0300

    added missing slash to order url pattern

--- partners/urls.py
+++ partners/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('upload/', upload_products, name='upload'),
     path('shop/', PartnerStateView.as_view(), name='shop'),
     path('shop/<int:pk>', PartnerStateView.as_view(), name='shop'),
-    path('order', PartnerOrders.as_view(), name='order')
+    path('order/', PartnerOrders.as_view(), name='order')
 ]
