ef4da723b4fbf44d1b3a14aa616116be137dec60
Ilya Lazarev <ilya.v.lazarev@gmail.com>
2021-08-18 13:35:42 +0300

    new fixtures added

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,3 +21,51 @@ def contact_factory():
         return baker.make("Contact", **kwargs)
 
     return factory
+
+
+@pytest.fixture()
+def shop_factory():
+    def factory(**kwargs):
+        return baker.make("Shop", **kwargs)
+
+    return factory
+
+
+@pytest.fixture()
+def order_factory():
+    def factory(**kwargs):
+        return baker.make("Order", **kwargs)
+
+    return factory
+
+
+@pytest.fixture()
+def product_factory():
+    def factory(**kwargs):
+        return baker.make("Product", **kwargs)
+
+    return factory
+
+
+@pytest.fixture()
+def category_factory():
+    def factory(**kwargs):
+        return baker.make("Category", **kwargs)
+
+    return factory
+
+
+@pytest.fixture()
+def product_info_factory():
+    def factory(**kwargs):
+        return baker.make("ProductInfo", **kwargs)
+
+    return factory
+
+
+@pytest.fixture()
+def order_item_factory():
+    def factory(**kwargs):
+        return baker.make("OrderItem", **kwargs)
+
+    return factory
